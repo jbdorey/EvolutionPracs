fff51e9bfa9d11816102cec65ca8f86edb420c7c
jbdorey <jbdorey@me.com>
2025-07-18 10:17:59 +1000

    interactive plots in Prac 3

--- R/DoreyGenbank.R
+++ R/DoreyGenbank.R
@@ -12,6 +12,7 @@
 #'
 #' @param db character, name of the database to search for.
 #' @param term character, the search term. The syntax used in making these searches is described in the Details of this help message, the package vignette and reference given below.
+#' @param retmax numeric. The maximum number of sequences to return and download in a single search.
 #' @param config vector configuration options passed to httr::GET
 #' @param retmode character, one of json (default) or xml. This will make no difference in most cases.
 #' @param use_history logical. If TRUE return a web_history object for use in later calls to the NCBI
